c6886e8f23a9d710f642d05b35e705dd7f283370
DanielaKrotofil <112739341+DanielaKrotofil@users.noreply.github.com>
2023-02-25 02:20:30 +0100

    portfolio part
    Changes:
    - HTML filled up
    - STYLE added
    - gallery added as a grid with JS modification
    
    + bonus: added buttons.scss as separate part for future using at other html

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {})
+document.addEventListener("DOMContentLoaded", function () {
+
+    const elements = document.getElementsByClassName("gallery__row__column");
+    const btn1 = document.getElementById("gallerybtn1");
+    const btn2 = document.querySelector("button#gallerybtn2");
+    const btn3 = document.querySelector("#gallerybtn3");
+
+    btn1.addEventListener("click", function () {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.flex = "100%";
+        }
+    })
+
+    btn2.addEventListener("click", function () {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.flex = "50%";
+          }
+    })
+
+    btn3.addEventListener("click", function () {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.flex = "25%";
+          }
+    })
+})
